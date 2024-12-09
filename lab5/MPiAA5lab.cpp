90b5dd1f94fb47b482d02ca907b5f2f14ceb3fbf
Vladimir <spasibomiryzazemfiry@gmail.com>
2024-12-09 23:54:05 +0700

    add lab5

--- lab5/MPiAA5lab.cpp
+++ lab5/MPiAA5lab.cpp
@@ -3,20 +3,20 @@
 #include <vector>
 #include <chrono>
 
-//Максимальное значение веса = 100
-#define INF 101 //(можно и MAX_INT поставить, но так просто числа меньше)
+//РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РІРµСЃР° = 100
+#define INF 101 //(РјРѕР¶РЅРѕ Рё MAX_INT РїРѕСЃС‚Р°РІРёС‚СЊ, РЅРѕ С‚Р°Рє РїСЂРѕСЃС‚Рѕ С‡РёСЃР»Р° РјРµРЅСЊС€Рµ)
 
 using namespace std;
 
-//функция для чтение матрицы смежности
-void ReadAdjacencyMatrix(const string& filename, vector<vector<int>>& adjMatrix, int& n) 
+//С„СѓРЅРєС†РёСЏ РґР»СЏ С‡С‚РµРЅРёРµ РјР°С‚СЂРёС†С‹ СЃРјРµР¶РЅРѕСЃС‚Рё
+void ReadAdjacencyMatrix(const string& filename, vector<vector<double>>& adjMatrix, int& n)
 {
    ifstream inputFile(filename);
 
-   inputFile >> n; // Чтение количества вершин(размер матрицы)
-   adjMatrix.resize(n, vector<int>(n));
+   inputFile >> n; // Р§С‚РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° РІРµСЂС€РёРЅ(СЂР°Р·РјРµСЂ РјР°С‚СЂРёС†С‹)
+   adjMatrix.resize(n, vector<double>(n));
 
-   // Чтение матрицы смежности
+   // Р§С‚РµРЅРёРµ РјР°С‚СЂРёС†С‹ СЃРјРµР¶РЅРѕСЃС‚Рё
    for (int i = 0; i < n; ++i)
       for (int j = 0; j < n; ++j)
          inputFile >> adjMatrix[i][j];
@@ -24,15 +24,15 @@ void ReadAdjacencyMatrix(const string& filename, vector<vector<int>>& adjMatrix,
    inputFile.close();
 }
 
-void ReadAdjacencyList(const string& filename, vector<vector<int>>& adjMatrix, int& n)
+void ReadAdjacencyList(const string& filename, vector<vector<double>>& adjMatrix, int& n)
 {
    ifstream inputFile(filename);
-   int m=0;
-   inputFile >> n >> m; // Чтение количества вершин(размер матрицы)
-   adjMatrix.resize(n, vector<int>(n));
+   int m = 0;
+   inputFile >> n >> m; // Р§С‚РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° РІРµСЂС€РёРЅ(СЂР°Р·РјРµСЂ РјР°С‚СЂРёС†С‹)
+   adjMatrix.resize(n, vector<double>(n));
    int u, v, w;
    for (int i = 0; i < n; i++)
-      for (int j = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
       {
          if (i == j)
             adjMatrix[i][j] = 0;
@@ -40,16 +40,16 @@ void ReadAdjacencyList(const string& filename, vector<vector<int>>& adjMatrix, i
             adjMatrix[i][j] = INF;
       }
 
-   // Чтение списка смежности и заполнение матрицы смежности
-   for (int i = 0; i < m; i++) 
+   // Р§С‚РµРЅРёРµ СЃРїРёСЃРєР° СЃРјРµР¶РЅРѕСЃС‚Рё Рё Р·Р°РїРѕР»РЅРµРЅРёРµ РјР°С‚СЂРёС†С‹ СЃРјРµР¶РЅРѕСЃС‚Рё
+   for (int i = 0; i < m; i++)
    {
       inputFile >> u >> v >> w;
-      adjMatrix[u-1][v-1] = w;
+      adjMatrix[u - 1][v - 1] = w;
    }
 }
 
-//вывести матрицу кратчайших путей в консоль
-void printPathsMatrix(const vector<vector<int>>& pathsMatrix, int& n)   
+//РІС‹РІРµСЃС‚Рё РјР°С‚СЂРёС†Сѓ РєСЂР°С‚С‡Р°Р№С€РёС… РїСѓС‚РµР№ РІ РєРѕРЅСЃРѕР»СЊ
+void printPathsMatrix(const vector<vector<double>>& pathsMatrix, int& n)
 {
    cout << "Shortest Path Matrix:" << endl;
    for (int i = 0; i < n; i++)
@@ -71,64 +71,99 @@ void printPathsMatrix(const vector<vector<int>>& pathsMatrix, int& n)
    }
 }
 
-//алгоритм флойда принимает матрицу смежности, количество вершин, количество потоков для OpenMP, возвращает матрицу кратчайших путей
-vector<vector<int>> Floyd(vector<vector<int>> adjMatrix, int& n, const int& num_of_threads)
+//Р°Р»РіРѕСЂРёС‚Рј С„Р»РѕР№РґР° РїСЂРёРЅРёРјР°РµС‚ РјР°С‚СЂРёС†Сѓ СЃРјРµР¶РЅРѕСЃС‚Рё, РєРѕР»РёС‡РµСЃС‚РІРѕ РІРµСЂС€РёРЅ, РєРѕР»РёС‡РµСЃС‚РІРѕ РїРѕС‚РѕРєРѕРІ РґР»СЏ OpenMP, РІРѕР·РІСЂР°С‰Р°РµС‚ РјР°С‚СЂРёС†Сѓ РєСЂР°С‚С‡Р°Р№С€РёС… РїСѓС‚РµР№
+vector<vector<double>> Floyd(vector<vector<double>> adjMatrix, vector<vector<double>>& path, int& n, const int& num_of_threads)
 {
    int i, j;
    for (int k = 0; k < n; k++)
    {
 #pragma omp parallel for num_threads(num_of_threads) private(j)
-      for (i = 0; i < n; i++)
+      for (i = 0; i < n; i++) {
+         if (adjMatrix[i][k] == INF)
+            continue;
          for (j = 0; j < n; j++)
          {
             if (i == j)
-               continue; // Пропустить самоциклы
+               continue; // РџСЂРѕРїСѓСЃС‚РёС‚СЊ СЃР°РјРѕС†РёРєР»С‹
             if (adjMatrix[i][k] + adjMatrix[k][j] < adjMatrix[i][j])
+            {
                adjMatrix[i][j] = adjMatrix[i][k] + adjMatrix[k][j];
+               path[i][j] = path[i][k];
+            }
          }
+      }
    }
    return adjMatrix;
 }
 
+void ShortestPath(const vector<vector<double>>& pathsMatrix, const vector<vector<double>>& path, int u, int v)
+{
+   if (pathsMatrix[u][v] == INF)
+      cout << "The Path does not exist";
+   else
+   {
+      int k = u;
+      while (k != v)
+      {
+         cout << k+1 <<" ";
+         k = path[k][v];
+      }
+      cout << k + 1;
+   }
+}
+
 int main()
 {
-   double start, finish, time;
-   vector<vector<int>> adjMatrix; //матрица смежности
-   int n = 0; //размер матрицы смежности
+   int u, v;
+   double start, finish;
+   vector<vector<double>> adjMatrix; //РјР°С‚СЂРёС†Р° СЃРјРµР¶РЅРѕСЃС‚Рё
+   vector<vector<double>> path; // РјР°С‚СЂРёС†Р° РєСЂР°С‚С‡Р°Р№С€РёС… РїСѓС‚РµР№
+   int n = 0; //СЂР°Р·РјРµСЂ РјР°С‚СЂРёС†С‹ СЃРјРµР¶РЅРѕСЃС‚Рё
    bool x = 1;
 
-   cout << "Choose matrix 1 or list 0: "; // выбрать тип входных данных
+   cout << "Choose matrix 1 or list 0: "; // РІС‹Р±СЂР°С‚СЊ С‚РёРї РІС…РѕРґРЅС‹С… РґР°РЅРЅС‹С…
    cin >> x;
    cout << endl;
 
    if (x)
    {
-      string filename = "matrix.txt"; // файл с матрицей смежности
+      string filename = "matrix.txt"; // С„Р°Р№Р» СЃ РјР°С‚СЂРёС†РµР№ СЃРјРµР¶РЅРѕСЃС‚Рё
       ReadAdjacencyMatrix(filename, adjMatrix, n);
    }
    else
    {
-      string filename = "list.txt"; // файл со списком смежности
+      string filename = "list.txt"; // С„Р°Р№Р» СЃРѕ СЃРїРёСЃРєРѕРј СЃРјРµР¶РЅРѕСЃС‚Рё
       ReadAdjacencyList(filename, adjMatrix, n);
    }
-   start = clock();
-   auto PathsMatrix1 = Floyd(adjMatrix, n, 1);
-   finish = clock();
-   cout << "time 1: " << finish - start << endl;
-   printPathsMatrix(PathsMatrix1, n);
+
+   path.resize(n, vector<double>(n));
+   for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+         path[i][j] = j;
 
    start = clock();
-   auto PathsMatrix2 = Floyd(adjMatrix, n, 2);
+   auto PathsMatrix1 = Floyd(adjMatrix, path,n, 1);
+   finish = clock();
+   cout << "time 1: " << finish - start << endl;
+   //printPathsMatrix(PathsMatrix1, n);
+
+   start = clock();
+   auto PathsMatrix2 = Floyd(adjMatrix, path, n, 2);
    finish = clock();
    cout << "time 2: " << finish - start << endl;
    //printPathsMatrix(PathsMatrix2, n);
 
    start = clock();
-   auto PathsMatrix8 = Floyd(adjMatrix, n, 8);
+   auto PathsMatrix8 = Floyd(adjMatrix, path , n, 8);
    finish = clock();
    cout << "time 8: " << finish - start << endl;
    //printPathsMatrix(PathsMatrix8, n);
 
-   //printPathsMatrix(Floyd(adjMatrix, n, 1), n);
+   printPathsMatrix(Floyd(adjMatrix, path, n, 1), n);
+
+   cout << "enter u v for path: " << endl;
+   cin >> u >> v;
+   ShortestPath(PathsMatrix1, path, u-1, v-1);
+
    return 0;
 }
